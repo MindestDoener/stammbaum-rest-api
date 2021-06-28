ada987cd53f110035c22e32217a7bd44c310579a
Justus Ernst <justernst.je@gmail.com>
2021-06-28 16:11:57 +0200

    initial setup of swagger ui

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
+
+const options = {
+    definition: {
+        info: {
+            title: "Stammbaum Rest-Api",
+            version: "1.0.0",
+            description:
+                "This is a Rest-Api for stammbaum-app",
+            contact: {
+                name: "Mindestdöner",
+                url: "https://github.com/MindestDoener",
+            },
+        },
+        servers: [
+            {
+                url: "http://localhost:3000/",
+            },
+        ],
+    },
+    apis: ["./app.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+);
 
 app.get('/', (req, res) => {
     res.send('Rest Api is Running');
