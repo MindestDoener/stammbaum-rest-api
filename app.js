182c02b6fa9eb23165cc9492d77c9f991c3ecea4
Chris <robinseifert0408@gmail.com>
2021-06-29 13:52:00 +0200

    testing post endpoint

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const client = require('./connection');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -10,25 +11,6 @@ const ENDPOINT = './endpoints/'
 app.use(require(ENDPOINT + 'swagger'));
 app.use(require(ENDPOINT + 'user'))
 
-const { Pool } = require('pg');
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
-app.get('/db', async (req, res) => {
-    try {
-        const client = await pool.connect();
-        const result = await client.query('SELECT * FROM USERTABLE');
-        const results = { 'results': (result) ? result.rows : null};
-        res.send(results);
-        client.release();
-    } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-    }
-});
 
 app.get('/', (req, res) => {
     res.send('Rest Api is Running');
@@ -42,4 +24,18 @@ app.listen(port, () => {
         console.log("Root: http://localhost:3000/");
         console.log("Docs: http://localhost:3000/api-docs");
     }
+});
+
+app.post('/users', (req, res)=> {
+    const user = req.body;
+    let insertQuery = `INSERT INTO USERTABLE(username, password) 
+                       values('${user.username}', '${user.password}')`
+
+    client.query(insertQuery, (err, result)=>{
+        if(!err){
+            res.send('Insertion was successful')
+        }
+        else{ console.log(err.message) }
+    });
+    client.end;
 });
