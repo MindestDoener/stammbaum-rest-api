51ca408b199c435abaec69a47715459661f8c77a
Chris <robinseifert0408@gmail.com>
2021-06-29 13:17:40 +0200

    adding test ep

--- app.js
+++ app.js
@@ -10,6 +10,26 @@ const ENDPOINT = './endpoints/'
 app.use(require(ENDPOINT + 'swagger'));
 app.use(require(ENDPOINT + 'user'))
 
+const { Pool } = require('pg');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+app.get('/db', async (req, res) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM USERTABLE');
+        const results = { 'results': (result) ? result.rows : null};
+        res.render('pages/db', results );
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
+});
+
 app.get('/', (req, res) => {
     res.send('Rest Api is Running');
 });
