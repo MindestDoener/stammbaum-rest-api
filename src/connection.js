85fd2646db1a9532954f35f78ae9b5655d84f1e8
Chris <robinseifert0408@gmail.com>
2022-02-17 11:48:11 +0100

    Adding Dockerfile

--- src/connection.js
+++ src/connection.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: "postgres://bznkpegeowwqaf:1430e0340808af1d579f80da831f766cf63b280bcd8c946ffde181fc5374aac6@ec2-54-220-170-192.eu-west-1.compute.amazonaws.com:5432/d7i4pv9ie2cu5p",
     ssl: {
         rejectUnauthorized: false
     }
